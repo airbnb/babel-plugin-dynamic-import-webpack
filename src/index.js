1172f90ce0947438d684125ab0bb7cfb962dd228
Jordan Gensler <jordan.gensler@airbnb.com>
2016-10-18 11:37:00 -0700

    Turning template into one-liner

--- src/index.js
+++ src/index.js
@@ -2,13 +2,7 @@ import template from 'babel-template';
 
 const TYPE_IMPORT = 'Import';
 
-const buildImport = template(`
-  (new Promise((resolve) => {
-    require.ensure([], (require) => {
-      resolve(require(SOURCE));
-    });
-  }))
-`);
+const buildImport = template('(new Promise((resolve) => { require.ensure([], (require) => { resolve(require(SOURCE)); }); }))');
 
 export default () => ({
   manipulateOptions(opts, parserOpts) {
