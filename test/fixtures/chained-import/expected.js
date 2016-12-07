0b8db4ef464ab5373868a30f00b4b95d4ed9eec9
Bradley Farias <bfarias@godaddy.com>
2016-12-07 09:28:52 -0600

    CJS interop changes

--- test/fixtures/chained-import/expected.js
+++ test/fixtures/chained-import/expected.js
@@ -1,23 +1,73 @@
 new Promise(resolve => {
-  require.ensure([], require => {
-    resolve(require('test-module'));
+  require.ensure(['test-module'], require => {
+    var ns = require('test-module');
+
+    if (ns && ns.__esModule) {
+      resolve(ns);
+    } else {
+      var wrap = Object.create(null);
+      wrap.__esModule = true;
+      wrap.default = ns;
+      Object.freeze(wrap);
+      resolve(wrap);
+    }
   });
 }).then(() => new Promise(resolve => {
-  require.ensure([], require => {
-    resolve(require('test-module-2'));
+  require.ensure(['test-module-2'], require => {
+    var ns = require('test-module-2');
+
+    if (ns && ns.__esModule) {
+      resolve(ns);
+    } else {
+      var wrap = Object.create(null);
+      wrap.__esModule = true;
+      wrap.default = ns;
+      Object.freeze(wrap);
+      resolve(wrap);
+    }
   });
 }));
 
 Promise.all([new Promise(resolve => {
-  require.ensure([], require => {
-    resolve(require('test-1'));
+  require.ensure(['test-1'], require => {
+    var ns = require('test-1');
+
+    if (ns && ns.__esModule) {
+      resolve(ns);
+    } else {
+      var wrap = Object.create(null);
+      wrap.__esModule = true;
+      wrap.default = ns;
+      Object.freeze(wrap);
+      resolve(wrap);
+    }
   });
 }), new Promise(resolve => {
-  require.ensure([], require => {
-    resolve(require('test-2'));
+  require.ensure(['test-2'], require => {
+    var ns = require('test-2');
+
+    if (ns && ns.__esModule) {
+      resolve(ns);
+    } else {
+      var wrap = Object.create(null);
+      wrap.__esModule = true;
+      wrap.default = ns;
+      Object.freeze(wrap);
+      resolve(wrap);
+    }
   });
 }), new Promise(resolve => {
-  require.ensure([], require => {
-    resolve(require('test-3'));
+  require.ensure(['test-3'], require => {
+    var ns = require('test-3');
+
+    if (ns && ns.__esModule) {
+      resolve(ns);
+    } else {
+      var wrap = Object.create(null);
+      wrap.__esModule = true;
+      wrap.default = ns;
+      Object.freeze(wrap);
+      resolve(wrap);
+    }
   });
 })]).then(() => {});
--- test/fixtures/dynamic-argument/expected.js
+++ test/fixtures/dynamic-argument/expected.js
@@ -1,12 +1,32 @@
 const MODULE = 'test-module';
 
 new Promise(resolve => {
-  require.ensure([], require => {
-    resolve(require(MODULE));
+  require.ensure([MODULE], require => {
+    var ns = require(MODULE);
+
+    if (ns && ns.__esModule) {
+      resolve(ns);
+    } else {
+      var wrap = Object.create(null);
+      wrap.__esModule = true;
+      wrap.default = ns;
+      Object.freeze(wrap);
+      resolve(wrap);
+    }
   });
 });
 new Promise(resolve => {
-  require.ensure([], require => {
-    resolve(require(`test-${MODULE}`));
+  require.ensure([`test-${ MODULE }`], require => {
+    var ns = require(`test-${ MODULE }`);
+
+    if (ns && ns.__esModule) {
+      resolve(ns);
+    } else {
+      var wrap = Object.create(null);
+      wrap.__esModule = true;
+      wrap.default = ns;
+      Object.freeze(wrap);
+      resolve(wrap);
+    }
   });
 });
