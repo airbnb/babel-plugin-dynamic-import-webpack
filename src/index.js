7b9fb2e964f196279441c9396a24dcab085c7504
Gary Borton <gary.borton@airbnb.com>
2017-06-20 17:17:36 -0700

    Utilize babelHelpers to ensure correct require result.

--- src/index.js
+++ src/index.js
@@ -6,19 +6,39 @@ const TYPE_IMPORT = 'Import';
 const buildImport = template(`
   (new Promise((resolve) => {
     require.ensure([], (require) => {
-      resolve(require(SOURCE));
+      requireLogic
     });
   }))
 `);
 
-export default () => ({
+export default ({ types }) => ({
   inherits: syntax,
 
   visitor: {
     CallExpression(path) {
       if (path.node.callee.type === TYPE_IMPORT) {
+        const importPath = path.node.arguments[0];
+        /**
+         * Create the resolve(require('path')) call.
+         * This utilizes babel-helpers to properly handle default and named exports.
+         */
+        const requireLogic = types.callExpression( // eslint-disable-line
+          types.identifier('resolve'), [
+            types.callExpression(
+              /**
+               * Wrap the require call with the require wildcard babel helper
+               * This helps to reduce boilerplate while sticking as closely
+               * as possible to the spec of import()
+               */
+              this.addHelper('interopRequireWildcard'), [
+                types.callExpression(types.identifier('require'), [importPath]),
+              ],
+            ),
+          ],
+        );
+
         const newImport = buildImport({
-          SOURCE: path.node.arguments,
+          requireLogic,
         });
         path.replaceWith(newImport);
       }
--- test/fixtures/basic-import/expected.js
+++ test/fixtures/basic-import/expected.js
@@ -1,5 +1,7 @@
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
 const testModule = new Promise(resolve => {
   require.ensure([], require => {
-    resolve(require('test-module'));
+    resolve(_interopRequireWildcard(require('test-module')));
   });
 });
